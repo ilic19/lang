38321462dc42cf1f9ed3b685010fe63e1f3227f9
ilic19 <78051224+ilic19@users.noreply.github.com>
2021-09-14 14:56:31 +0200

    Update auth.php

--- locales/sr_Cyrl/auth.php
+++ locales/sr_Cyrl/auth.php
@@ -13,6 +13,6 @@
 
 return [
     'failed'   => 'Подаци не одговарају ни једном налогу.',
-    'password' => 'The provided password is incorrect.',
+    'password' => 'Нетачна лозинка.',
     'throttle' => 'Превише неуспелих покушаја. Покушајте поново за :seconds секунди.',
 ];
